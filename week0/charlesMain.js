439fa653cab993a27fb75e8bb97bc279c4087a63
unknown <onahcharles1995@gmail.com>
2019-03-26 21:17:54 +0800

    My inital commit

---
+++ week0/charlesMain.js
@@ -0,0 +1,8 @@
+console.log("Hello word")
+console.log("I am a string. I must be inside the double appostrophe")
+console.log("12345")
+var name  = Charles
+console.log("my name is, name");
+var age = 13;
+var score = 45;
+console.log("my score and my age total is, age + score");
