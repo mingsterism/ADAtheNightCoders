c4a610c4c770aa14472c345848029f0898cc215e
Mongie18 <mongkarmun@hotmail.com>
2019-03-26 21:24:25 +0800

    My New File

---
+++ week0/MongieMain.js
@@ -0,0 +1,17 @@
+console.log("no strings on me")
+console.log((52*42 > 0))
+var name = "Ted"
+console.log("My name is", name)
+var land1 = 8
+var land2 = 2
+console.log("the number of the land", (land1 + land2) > 0)
+var sea1 = 0
+var sea2 = -2
+console.log("the number of the sea is", (sea1 + sea2) >0)
+var THEFATCAT = 28
+var score = 100
+var age = 8
+var total = age + score
+name = "Ted"
+console.log(name score) "is" score "and my" age total "is", age + score)
+console.log("my score and my age total is", total)
