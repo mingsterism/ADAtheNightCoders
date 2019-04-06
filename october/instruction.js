d4145911025b9aee73ee7a09259944d926222c0c
WeiNing2048 <wenyhuang17@gmail.com>
2019-04-06 10:16:20 +0800

    add item

---
+++ october/instruction.js
@@ -0,0 +1,35 @@
+//cd (change directory)
+//cd D: (direct to Drive D)
+//cd ..
+//click Tab to show option
+//ls (list folders)
+//mkdir (make new directory)
+//rmdir ()
+//git clone http://
+//git checkout -b (create new branch)
+//clear (clear the record)
+//git checkout (direct to other branch)
+//git checkout master (direct to master branch)
+//git add . (if push in my branch, if pull in master branch)
+//git commit -am 'My new files' (Key in what have updated)
+//git push (launch to github)
+//git push --set-upstream origin weining-branch
+//git fetch --all (fetches the latest update from github)
+//git branch -r (see all latest branches)
+//git rebase master
+
+//"vim"
+//click "i" to insert
+//click "Esc" to change to control keypad (up"K" down"J" left"H" right"L")
+//:q! (quit without saving)
+//:q (quit & saved)
+
+//touch week1/superfile
+//mv superfile ../week2
+
+//var a = 15
+//function roo(inside the box){
+// console.log(say something privately)
+// return(shout out something);
+//}
+//console.log(roo(something to say))
