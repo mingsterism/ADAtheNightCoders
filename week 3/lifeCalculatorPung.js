f4debdebc5235b7e7e996cf9e15bfa0e56f5b32b
batcoder <wypung92@gmail.com>
2019-04-22 22:35:56 +0800

    pung week 4

---
+++ week 3/lifeCalculatorPung.js
@@ -0,0 +1,11 @@
+/**
+ * Calculate your savings by the tiime you reach 50
+ * years of working from 25 - 50 = 25 years
+ * yearly income
+ * yearly expense
+ * yearly investment
+ * return of investment per year
+ * yearly income = yearly income - yearly expenses - yearly investment + (yearly investment + *return of investment per year)]* 25 years/ 
+ **/
+
+//  fixexpenses mortgage & 
---
+++ week 3/readlinePung.js
@@ -0,0 +1,35 @@
+// const readline = require('readline');
+
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+
+// rl.question('What do you think of Node.js? ', (answer) => {
+//   // TODO: Log the answer in a database
+//   console.log(`Thank you for your valuable feedback: ${answer}`);
+
+//   rl.close();
+// });
+
+class Batman {
+  constructor (hp, strenght, money,) {
+    this.hp = hp;
+    this.strenght = strenght;
+    this.money = money;
+  }
+
+  attack() {
+    this.money = this.money - 10;
+  }
+
+  attacked() {
+    this.hp = this.hp - 10;
+  }
+  superAttack( )
+}
+  var batman = new Batman(100, 100, 100);
+  batman.attack()
+  batman.attacked()
+  console.log ("batman money left ", batman.money);
+  console.log ("batman hp left", batman.hp);
